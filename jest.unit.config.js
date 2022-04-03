2dbb7c06482bddf62a57990cdff92ef6822c3945
licege <warraven53@gmail.com>
2022-04-03 19:54:55 +0200

    Add jest config

---
+++ jest.unit.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  displayName: 'unit',
+  testEnvironment: 'jsdom',
+  roots: [path.join(__dirname, 'src')],
+  setupFilesAfterEnv: [path.join(__dirname, 'src/testing/setup.ts')],
+  // collectCoverage: true,
+  // collectCoverageFrom: ['src/*/**/*.{ts,tsx}'],
+  // coveragePathIgnorePatterns: [
+  //   "\\.d\\.ts$",
+  //   "\\.e2e\\.tsx?$",
+  //   "src/types",
+  //   "src/testing",
+  // ]
+}
