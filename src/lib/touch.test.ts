55720b1329160ea0cb9ba3a650a6cc23ca7bfffb
licege <warraven53@gmail.com>
2022-04-05 20:10:48 +0200

    Add test for touch lib

---
+++ src/lib/touch.test.ts
@@ -0,0 +1,42 @@
+import { coordX, coordY, TXUITouchEvent } from './touch';
+
+// Настоящего Touch нет в jsdom: https://github.com/jsdom/jsdom/issues/1508
+const touch = (ops: Partial<TouchInit>) => ops as Touch;
+
+describe('touch', () => {
+  describe.each(['coordX', 'coordY'] as const)('%s', (getterName) => {
+    const getter = getterName === 'coordX' ? coordX : coordY;
+
+    it('works with mouse', () => {
+      expect(
+        getter(
+          new MouseEvent('mousemove', {
+            clientX: 100,
+            clientY: 100
+          }) as TXUITouchEvent
+        )
+      ).toBe(100);
+    });
+
+    it('works with touch', () => {
+      expect(
+        getter(
+          new TouchEvent('touchmove', {
+            changedTouches: [touch({ clientX: 100, clientY: 100 })],
+          }) as TXUITouchEvent
+        )
+      ).toBe(100);
+    });
+
+    it('works with mouse when client = 0', () => {
+      expect(
+        getter(
+          new MouseEvent('mousemove', {
+            clientX: 0,
+            clientY: 0
+          }) as TXUITouchEvent
+        )
+      ).toBe(0);
+    });
+  });
+});
